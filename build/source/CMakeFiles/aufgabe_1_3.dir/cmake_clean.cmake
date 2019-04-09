e224e0c34ee36f2ee8f132c7d1e4dcd0035af72a
Jonas Roquette <roquettejonas@gmail.com>
2019-04-09 09:26:59 +0200

    uebung

---
+++ build/source/CMakeFiles/aufgabe_1_3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aufgabe_1_3.dir/aufgabe_1.3.cpp.o"
+  "aufgabe_1_3.pdb"
+  "aufgabe_1_3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aufgabe_1_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- source/tests.cpp
+++ source/tests.cpp
@@ -1,5 +1,5 @@
 #define CATCH_CONFIG_RUNNER
-#include "catch.hpp"
+#include <catch.hpp>
 #include <cmath>
 
 unsigned int gcd(unsigned int a, unsigned int b)
@@ -19,7 +19,7 @@ TEST_CASE("describe_gcd", "[gcd]")
     REQUIRE(gcd (9 ,6) == 3);
     REQUIRE(gcd (3 ,7) == 1);
 }
-
+ 
 int main(int argc, char* argv[])
 {
   return Catch::Session().run(argc, argv);
